072a4ed68c7a1f01381c99860f74c16441f67d5f
MalekNaab <58034636+MalekNaab@users.noreply.github.com>
2022-11-17 22:19:29 +0000

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,55 @@
+export var Classes = [
+    // each object represent a product item, for more products just add more objects
+    {
+        // id: id++,
+        image: "img/English.png",
+        Subject: 'English',
+        Location: 'London',
+        Price: 100.00,
+        Spaces: 5
+    },
+    {
+        // id: id++,
+        image: "img/Maths.png",
+        Subject: 'Maths',
+        Location: 'Essex',
+        Price: 95.00,
+        Spaces: 6
+    },
+
+    {
+        // id: id++,
+        image: "img/Science.png", 
+        Subject: 'Science', 
+        Location: 'Oxford', 
+        Price: 60.00, 
+        Spaces: 7
+    },
+
+    {
+        // id: id++, 
+        image: "img/Music.png", 
+        Subject: 'Music', 
+        Location: 'London', 
+        Price: 70.00, 
+        Spaces: 8
+    },
+
+    {
+        // id: id++, 
+        image: "img/Art.png", 
+        Subject: 'Art', 
+        Location: 'Oxford', 
+        Price: 80.00, 
+        Spaces: 9
+    },
+
+    {
+        // id: id++, 
+        image: "img/Sport.png", 
+        Subject: 'Sport', 
+        Location: 'Essex', 
+        Price: 50.00, 
+        Spaces: 10
+    },
+]
