ea0443e5d4ea3ebbbc0be40048165abda9106696
MalekNaab <58034636+MalekNaab@users.noreply.github.com>
2022-11-26 17:35:36 +0000

    Update classes.js
    Signed-off-by: MalekNaab <58034636+MalekNaab@users.noreply.github.com>

--- classes.js
+++ classes.js
@@ -1,7 +1,7 @@
 export var Classes = [
     // each object represent a product item, for more products just add more objects
     {
-        // id: id++,
+        id: '1001',
         image: "img/English.png",
         Subject: 'English',
         Location: 'London',
@@ -9,43 +9,39 @@ export var Classes = [
         Spaces: 5
     },
     {
-        // id: id++,
+        id: '1002',
         image: "img/Maths.png",
         Subject: 'Maths',
         Location: 'Essex',
         Price: 95.00,
         Spaces: 5
     },
-
     {
-        // id: id++,
+        id: '1003',
         image: "img/Science.png", 
         Subject: 'Science', 
         Location: 'Oxford', 
         Price: 60.00, 
         Spaces: 5
     },
-
     {
-        // id: id++, 
+        id: '1004', 
         image: "img/Music.png", 
         Subject: 'Music', 
         Location: 'London', 
         Price: 70.00, 
         Spaces: 5
     },
-
     {
-        // id: id++, 
+        id: '1005', 
         image: "img/Art.png", 
         Subject: 'Art', 
         Location: 'Oxford', 
         Price: 80.00, 
         Spaces: 5
     },
-
     {
-        // id: id++, 
+        id: '1006', 
         image: "img/Sport.png", 
         Subject: 'Sport', 
         Location: 'Essex', 
